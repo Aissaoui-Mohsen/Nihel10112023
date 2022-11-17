d1a7422721865fb9351e9da711e54d6929940e83
Nada Farah <nada.farah@neolians.com>
2022-11-17 10:37:14 +0100

    Vérify Nos Dernière Publications Page Home

--- src/main/java/com/neolians/website/repository/pages/PageHome.java
+++ src/main/java/com/neolians/website/repository/pages/PageHome.java
@@ -1,5 +1,7 @@
 package com.neolians.website.repository.pages;
 
+import com.neolians.common.utils.selenium.elements.BlockElement;
+import com.neolians.common.utils.selenium.elements.LinkElement;
 import com.neolians.common.utils.selenium.elements.UrlElement;
 import org.openqa.selenium.By;
 
@@ -13,5 +15,9 @@ public class PageHome {
 	public static final UrlElement url = new UrlElement("Home url", "/");
 	public static final ButtonElement paragraph1 = new ButtonElement("Paragraphe1",
 			By.xpath("//p[contains(text(),'Notre expertise d’industrialisation de test ']"));
+   public static final BlockElement enteprise=new BlockElement("L'enteprise",By.xpath("//li[@id='menu-item-26120']"));
+	public static final BlockElement Nosdernièrespublications=new BlockElement("Nos dernières publications",By.xpath("//div[@data-id='f70bbd3']//child::div//h2"));
+
+
 
 }
